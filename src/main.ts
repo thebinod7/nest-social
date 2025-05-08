841f3b12ad450856ed9d769cca4dc5f566b693a2
Binod Chaudhary <thebinod7@gmail.com>
2025-05-08 06:27:05 +0545

    added swagger API

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+const PORT = process.env.PORT || 8085;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors();
+  const globalPrefix = 'api/v1';
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
+  app.setGlobalPrefix(globalPrefix);
+
+  const config = new DocumentBuilder()
+    .setTitle('Social App')
+    .setDescription('API server for Social App')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
+  await app.listen(PORT);
+  Logger.log(`Swagger UI: http://localhost:${PORT}/swagger`);
 }
 bootstrap();
